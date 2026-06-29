d4332241ea9cfdbf7761e31c41c83f60e10fb1a8
Damian Mendel <mendeldamian01@gmail.com>
2026-06-29 17:57:06 +0200

    feat(metrics): add run metrics schema

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,6 @@
 from models.persona import Persona
 from models.persona_run import PersonaRun
 from models.run import Run
+from models.run_metrics import RunMetrics
 
-__all__ = ["Persona", "Run", "PersonaRun"]
+__all__ = ["Persona", "Run", "PersonaRun", "RunMetrics"]
